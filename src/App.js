bbd448fbb72f7028493f5d39ff2f7e337a22eebd
Muhammad Shahzad <abisim32@gmail.com>
2020-06-04 01:49:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Shahzad had edited <code>src/App.js</code> and save to reload.
+          Muhammad Shahzad had edited <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
