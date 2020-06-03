19563eeee0b5c467976c64a58dcefcbec30f9df2
Muhammad Shahzad <abisim32@gmail.com>
2020-06-04 02:34:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Muhammad Shahzad student of BootCamp2020 (urdu)had edited <code>src/App.js</code> and save to reload.
+          Muhammad Shahzad student of BootCamp2020 (urdu) 
+          had edited <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
